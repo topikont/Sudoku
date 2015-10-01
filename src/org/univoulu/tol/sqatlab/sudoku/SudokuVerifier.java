7770ae6d06329a42eac814915343ee8fa0dba39d
somename <someemail>
2015-10-01 11:54:19 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -4,19 +4,25 @@ public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		if(!checkRow(candidateSolution)) {
-			
+			return -3;
 		}
 		return 1;
 	}
 	
-	private bool checkRow(String candidateSolution) {
+	private Boolean checkRow(String candidateSolution) {
 		
 		for(int i = 0; i < 9; i++) {
 			int beginIndex = i * 9;
 			int endIndex = beginIndex + 9;
 			String subString = candidateSolution.substring(beginIndex, endIndex);
+			
+			
 		}
 		
 		return true;
+	}
+	
+	private Boolean checkSubString() {
+		
 	}
 }
