ee2755ef52983535867137ea1fe5a730927273d3
topikont <topikont@STUPID-PC508.stnet.local>
2015-10-01 13:45:52 +0300

    sgr

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.xml.internal.ws.policy.spi.AssertionCreationException;
-
 public class TestSudokuVerifier {
 
 	private SudokuVerifier verifier;
