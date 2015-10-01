d7ef69e1eed8f36b3597e3b56979bbf2dcdefa72
somename <someemail>
2015-10-01 12:34:29 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -4,7 +4,7 @@ public class SudokuVerifier {
 
 	public static final int RESULT_CORRECT = 0;
 	public static final int RESULT_INCORRECT_RULE_1 = -1;
-	public static final int RESULT_INCORRECT_RULE_2 = -2;
+	public static final int RESULT_INCORRECT_SUBGRID_PROBLEM = -2;
 	public static final int RESULT_INCORRECT_RULE_3 = -3;
 	public static final int RESULT_INCORRECT_RULE_4 = -4;
 	public static final int RESULT_INCORRECT_STRING_TOO_SHORT = -5;
